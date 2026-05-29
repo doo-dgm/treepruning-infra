d401668eaf55546a11500a0021c5d4afd874a7f9
Danilo Cordoba <118949682+zha808@users.noreply.github.com>
2026-05-29 01:59:04 -0500

    Update pgdump-to-mysql.sh

--- docker/scripts/pgdump-to-mysql.sh
+++ docker/scripts/pgdump-to-mysql.sh
@@ -91,6 +91,8 @@ docker exec -e PGPASSWORD="$POSTGRES_PASSWORD" pg1 \
     -e 's/"public"\.//g' \
     -e "s/'true'/1/g" \
     -e "s/'false'/0/g" \
+    -e "s/'{}'/'\[\]'/g" \
+    -e "s/'{\([^}]*\)}'/'[\1]'/g" \
 | sed -E \
     -e 's/::[a-zA-Z_ ]+(\[\])?//g' \
     -e "s/\btrue\b/1/g" \
